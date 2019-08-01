7e478e6a6082c7757310ce7e341b8f07c3b7a085
liChenYang <chenyang.li@dmall.com>
2019-08-01 17:05:22 +0800

    operators

--- Operators.dart
+++ Operators.dart
@@ -15,6 +15,11 @@ void main(List<String> args) {
   int b = 2;
   print(a ~/ b);
   Person p = new Person();
-  p..name='wang'..setCountry('China');
+  p
+    ..name = 'wang'
+    ..setCountry('China');
   print(p);
+  // 使用 = 操作符来赋值。但是还有一个 ??= 操作符用来指定值为 null 的变量的值。
+  b ??= a;
+  // 如果布尔表达式是测试值是否为null，考虑使用 ??
 }
