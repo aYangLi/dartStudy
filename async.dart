2d90ca16ee8fa453ade516904515c51ede37d16c
liChenYang <chenyang.li@dmall.com>
2019-08-07 05:16:52 +0800

    env

---
+++ async.dart
@@ -0,0 +1,15 @@
+//Dart 有一些语言特性来支持 异步编程。 最常见的特性是 async 方法和 await 表达式。
+//
+//Dart 库中有很多返回 Future 或者 Stream 对象的方法。 这些方法是 异步的： 这些函数在设置完基本的操作 后就返回了， 而无需等待操作执行完成。 例如读取一个文件，在打开文件后就返回了。
+
+//有两种方式可以使用 Future 对象中的 数据：
+//
+//使用 async 和 await
+//使用 Future API
+//同样，从 Stream 中获取数据也有两种 方式：
+//
+//使用 async 和一个 异步 for 循环 (await for)
+//使用 Stream API
+void main() {
+
+}
